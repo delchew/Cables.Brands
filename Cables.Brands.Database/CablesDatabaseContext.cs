09d74e27a80a58ec7667213f99ee26cad7673a00
Алексей Бондаренко <delchew@gmail.com>
2021-01-14 17:36:31 +0300

    Developing

--- Cables.Brands.Database/CablesDatabaseContext.cs
+++ Cables.Brands.Database/CablesDatabaseContext.cs
@@ -1,32 +1,48 @@
 ﻿using Cables.Brands.Common;
 using FirebirdSql.Data.FirebirdClient;
+using FirebirdSql.EntityFrameworkCore;
+using FirebirdSql.EntityFrameworkCore.Firebird;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Cables.Brands.Database
 {
     public class CablesDatabaseContext : DbContext
     {
         public DbSet<FireProtectionClass> FireProtectionClasses { get; set; }
-
-        public CablesDatabaseContext()
-        {
-            Database.EnsureCreated();
-        }
+        //public CablesDatabaseContext()
+        //{
+        //    Database.EnsureCreated();
+        //}
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            base.OnConfiguring(optionsBuilder);
+
             var builder = new FbConnectionStringBuilder
             {
                 Charset = "none",
                 UserID = "SYSDBA",
                 Password = "masterkey",
                 Dialect = 3,
-                Database = @"Cables.Brands.FDB",
                 DataSource = "localhost",
                 Port = 3050,
+                Database = @"E:\Distr\firebird\CablesBrands.FDB"
+
             };
-            var connection = new FbConnection(builder.ToString());
-            optionsBuilder.UseFirebird(connection);
+
+            optionsBuilder.UseFirebird(builder.ToString());
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            var builder = modelBuilder.Entity<FireProtectionClass>();
+            builder.Property(x => x.Id).HasColumnName("ID");
+            builder.Property(x => x.ProtectionClass).HasColumnName("PROTECTION_CLASS");
+            builder.Property(x => x.Designation).HasColumnName("DESIGNATION");
+            builder.ToTable("FIRE_PROTECTION_CLASSES");
         }
     }
 }
