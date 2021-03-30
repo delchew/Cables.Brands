eb9ca55dd63837cafbed70702be1b8e6af6b1b73
delchew <delchew@mail.ru>
2021-03-30 18:05:41 +0300

    Add FlatCableSize Entity

---
+++ CablesDatabaseEFCoreFirebird/Entities/FlatCableSize.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CablesDatabaseEFCoreFirebird.Entities
+{
+    public class FlatCableSize
+    {
+        public int Id { get; set; }
+
+
+    }
+}
